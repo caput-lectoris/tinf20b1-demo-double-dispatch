551784a9400e339aa6eac810095b417afe507776
CaputHominis <19929725+CaputHominis@users.noreply.github.com>
2022-05-10 00:04:42 +0200

    error: every node/instruction has a type
    This commit does not compile! It serves to illustrate a violation of the
    (polymorphic) open/closed principle. Adding an abstract method to
    ArithmeticExpression causes compile errors in all subclasses.

--- src/main/java/de/dhbw/caput/tinf20b1/ll1/ArithmeticExpression.java
+++ src/main/java/de/dhbw/caput/tinf20b1/ll1/ArithmeticExpression.java
@@ -2,10 +2,13 @@ package de.dhbw.caput.tinf20b1.ll1;
 
 abstract class ArithmeticExpression {
 	
+	protected Datatype type;
+	
 	ArithmeticExpression(){
 		super( );
 	}
 	
 	abstract String generateJavaBytecode( );
+	abstract Datatype inferTypes( );
 	
 }
